e997c21769d6f946aad1fe7005fd620a213c6e3a
LejlaMumi <47846805+LejlaMumi@users.noreply.github.com>
2019-04-30 13:10:09 +0200

    Add files via upload

---
+++ Count/Simplefocus_Count.py
@@ -0,0 +1,21 @@
+import pymysql
+
+mydb= pymysql.connect(host='localhost',
+        user='root',
+        password='password',
+        db='food')
+
+mycursor = mydb.cursor()
+
+sql = "SELECT Player_id, " \
+      "COUNT(simplefocusstatistic_id) " \
+      "FROM simplefocusstatistic_studentanswers " \
+      "WHERE Player_id>2 GROUP BY Player_id"
+
+
+mycursor.execute (sql)
+
+myresults = mycursor.fetchall()
+
+#for result in myresults :
+ #   print("Student ID: ", result[0], "     ", "Number of Q: ", result[1])
